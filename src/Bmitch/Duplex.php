1e8b5857b4e50bd41a6f16d5e9e2e707931d1f1e
Bill Mitchell <wkmitch@gmail.com>
2016-01-19 18:16:47 -0800

    Ignore common words

--- src/Bmitch/Duplex.php
+++ src/Bmitch/Duplex.php
@@ -4,6 +4,12 @@ namespace Bmitch;
 
 class Duplex
 {
+	protected $commonWords = [
+		'the',
+		'of',
+		'a',
+		'and'
+	];
 
 	public static function make()
 	{
@@ -15,14 +21,40 @@ class Duplex
 		return $phrase01 === $phrase02;
 	}
 	
-	public static function matchPercent($phrase01, $phrase02)
+	public function matchPercent($phrase01, $phrase02)
 	{
 		$phrase01Array = explode(' ', $phrase01);
 		$phrase02Array = explode(' ', $phrase02);
-		$diff =  array_diff($phrase01Array, $phrase02Array);
+
+		$phrase01Array = $this->cleanse($phrase01Array);
+		$phrase02Array = $this->cleanse($phrase02Array);
+
+		$diff = array_diff($phrase01Array, $phrase02Array);
 
 		$percentage = count($diff)/count($phrase01Array);
 
 		return (1 - $percentage) * 100;
+	}
+
+	private function cleanse(array $phraseArray)
+	{
+		foreach($phraseArray as &$word) {
+			$word = strtolower($word);
+		}
+
+		$phraseArray = $this->removeCommonWords($phraseArray);
+
+		return $phraseArray;
+	}
+
+	private function removeCommonWords(array $phraseArray)
+	{
+		foreach ($phraseArray as $index => $word) {
+			if (in_array($word, $this->commonWords)) {
+				unset($phraseArray[$index]);
+			}
+		}
+
+		return $phraseArray;
 	}
 }
--- tests/DuplexTest.php
+++ tests/DuplexTest.php
@@ -25,6 +25,11 @@ class DuplexTest extends PHPUnit_Framework_TestCase
 		$phrase02 = 'one two four five';
 		$match = Duplex::make()->matchPercent($phrase01, $phrase02);
 		$this->assertEquals(75, $match);
+
+		$phrase01 = 'one two three four five six seven eight';
+		$phrase02 = 'three four five six seven eight';
+		$match = Duplex::make()->matchPercent($phrase01, $phrase02);
+		$this->assertEquals(75, $match);
 	}
 
 	/** @test */
@@ -32,6 +37,39 @@ class DuplexTest extends PHPUnit_Framework_TestCase
 	{
 		$phrase01 = 'one two three four';
 		$phrase02 = 'one two five six';
+		$match = Duplex::make()->matchPercent($phrase01, $phrase02);
+		$this->assertEquals(50, $match);
+
+		$phrase01 = 'five six seven eight';
+		$phrase02 = 'seven eight nine ten';
+		$match = Duplex::make()->matchPercent($phrase01, $phrase02);
+		$this->assertEquals(50, $match);
+	}
+
+	/** @test */
+	public function it_is_case_insensitive()
+	{
+		$phrase01 = 'one two three four';
+		$phrase02 = 'ONE TWo FOUr fivE';
+		$match = Duplex::make()->matchPercent($phrase01, $phrase02);
+		$this->assertEquals(75, $match);
+
+		$phrase01 = 'five six seven eight';
+		$phrase02 = 'SEVEN EIGHt NIne teN';
+		$match = Duplex::make()->matchPercent($phrase01, $phrase02);
+		$this->assertEquals(50, $match);
+	}
+
+	/** @test */
+	public function it_ignores_common_words()
+	{
+		$phrase01 = 'The one a two and three of four';
+		$phrase02 = 'ONE TWo FOUr fivE';
+		$match = Duplex::make()->matchPercent($phrase01, $phrase02);
+		$this->assertEquals(75, $match);
+
+		$phrase01 = 'Of five and six a seven the eight';
+		$phrase02 = 'SEVEN EIGHt NIne teN';
 		$match = Duplex::make()->matchPercent($phrase01, $phrase02);
 		$this->assertEquals(50, $match);
 	}
